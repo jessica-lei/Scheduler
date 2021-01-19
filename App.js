147648f91034c26577499ea1572d2ae2f07b843e
Jessica Lei <jessicalei2022@u.northwestern.edu>
2021-01-19 13:00:18 -0600

    title display

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 const schedule = {
-  title: "CS Courses for 2020 - 2021"
+  title: "CS Courses for 2018-2019"
 };
 
 const App = () => {
@@ -26,3 +26,15 @@ const styles = StyleSheet.create({
 });
 
 export default App;
+
+/*
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="/__/firebase/8.2.3/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="/__/firebase/8.2.3/firebase-analytics.js"></script>
+
+<!-- Initialize Firebase -->
+<script src="/__/firebase/init.js"></script>
+*/
